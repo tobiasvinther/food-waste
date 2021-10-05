6a5795ee64e4114cfeeaeb0dbb2db6c4b3aba743
tobiasvinther <tobi626i@stud.kea.dk>
2021-10-05 15:28:25 +0200

    first commit

---
+++ src/main/java/com/example/foodwaste/FoodWasteApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodwaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodWasteApplication.class, args);
+    }
+
+}
